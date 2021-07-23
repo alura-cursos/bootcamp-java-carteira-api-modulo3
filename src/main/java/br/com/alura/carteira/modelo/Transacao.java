5a2d8c74a982a0921405abfc745b2a9d3b5fbe56
Rodrigo Ferreira <r.caneppele@gmail.com>
2021-07-23 15:35:17 -0300

    codigos da aula 3

--- src/main/java/br/com/alura/carteira/modelo/Transacao.java
+++ src/main/java/br/com/alura/carteira/modelo/Transacao.java
@@ -3,6 +3,15 @@ package br.com.alura.carteira.modelo;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,12 +23,22 @@ import lombok.ToString;
 @ToString(exclude = {"data", "quantidade", "tipo"})
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "transacoes")
 public class Transacao {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String ticker;
 	private LocalDate data;
 	private BigDecimal preco;
-	private int quantidade;
+	private Integer quantidade;
+	
+	@Enumerated(EnumType.STRING)
 	private TipoTransacao tipo;
+	
+	@ManyToOne
+	private Usuario usuario;
 
 }
--- src/main/java/br/com/alura/carteira/modelo/Usuario.java
+++ src/main/java/br/com/alura/carteira/modelo/Usuario.java
@@ -1,5 +1,11 @@
 package br.com.alura.carteira.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +17,13 @@ import lombok.ToString;
 @ToString(exclude = {"senha"})
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String login;
 	private String senha;
